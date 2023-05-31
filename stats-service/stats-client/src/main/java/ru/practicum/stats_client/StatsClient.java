c6a7a4140be9bb256fff3487461442868e8f32e7
Ilusha92 <Ushakov@ruvents.com>
2023-05-31 15:18:14 +0300

    Update StatsClient.java

--- stats-service/stats-client/src/main/java/ru/practicum/stats_client/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/stats_client/StatsClient.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
